8d67d5df02f8966c3bc2eb4e91abb7d77e243a19
Patrick Drechsler <patrick@pdrechsler.de>
2011-05-29 12:11:14 +0200

    Working index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,57 +1,62 @@
  class UsersController < ApplicationController
 
-  before_filter :authenticate, :only => [:edit, :update]
-  before_filter :correct_user, :only => [:edit, :update]
+   before_filter :authenticate, :only => [:index, :edit, :update]
+   before_filter :correct_user, :only => [:edit, :update]
 
-  def show
-    @user = User.find(params[:id])
-    @title = @user.name
-  end
+   def index
+     @users = User.all 
+     @title = "All users"
+   end
 
-  def new
-    @user = User.new
-    @title = "Sign up"
-  end
+   def show
+     @user = User.find(params[:id])
+     @title = @user.name
+   end
 
-  def create
-    @user = User.new(params[:user])
-    if @user.save
-      sign_in @user 
-      redirect_to @user, :flash => { :success => "Welcome to AME!" }
-    else
-      @title = "Sign up"
-      render :new
-    end
-  end
+   def new
+     @user = User.new
+     @title = "Sign up"
+   end
 
-  def edit
-    @user = User.find(params[:id])
-    @title = "Edit user"
-  end
+   def create
+     @user = User.new(params[:user])
+     if @user.save
+       sign_in @user 
+       redirect_to @user, :flash => { :success => "Welcome to AME!" }
+     else
+       @title = "Sign up"
+       render :new
+     end
+   end
 
-  def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(params[:user])
-      redirect_to @user, :flash => { :success => "Profile updated" }
-    else
-      @title = "Edit user"
-      render :edit
-    end
-  end
+   def edit
+     @user = User.find(params[:id])
+     @title = "Edit user"
+   end
 
-  private
+   def update
+     @user = User.find(params[:id])
+     if @user.update_attributes(params[:user])
+       redirect_to @user, :flash => { :success => "Profile updated" }
+     else
+       @title = "Edit user"
+       render :edit
+     end
+   end
 
-  def authenticate
+   private
+
+   def authenticate
      deny_access unless signed_in?
-  end
+   end
 
-  # Prevent user A from editing settings of user B
-  def correct_user
-    # get requested user from url params...
-    @user = User.find(params[:id])
-    # ...and compare to current user
-    redirect_to root_path unless current_user?(@user)
-  end
+   # Prevent user A from editing settings of user B
+   def correct_user
+     # get requested user from url params...
+     @user = User.find(params[:id])
+     # ...and compare to current user
+     redirect_to root_path unless current_user?(@user)
+   end
 
-end
+ end
 
